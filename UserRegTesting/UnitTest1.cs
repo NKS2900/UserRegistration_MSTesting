755672ec55a80a2bd7f3e60c9e0ee0723d0551f0
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-10 09:25:05 +0530

    [NIJAM] Add:Password Testing.

--- UserRegTesting/UnitTest1.cs
+++ UserRegTesting/UnitTest1.cs
@@ -17,5 +17,33 @@ namespace UserRegTesting
             bool result = program.FirstNameVal(name);
             Assert.AreEqual(true, result);
         }
+        [TestMethod]
+        public void LastNameTest()
+        {
+            string name = "Sayyad";
+            bool result = program.LastNameVal(name);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void EmailTest()
+        {
+            string email = "nks.nks@gmail.com.in";
+            bool result = program.EmailValidation(email);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void MobileTest()
+        {
+            string mobile = "91 9130512900";
+            bool result = program.MobileValidation(mobile);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void PasswordTest()
+        {
+            string password = "Console@123";
+            bool result = program.PasswordRule(password);
+            Assert.AreEqual(true, result);
+        }
     }
 }
--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -12,11 +12,50 @@ namespace UserRegistration
             Console.Write("Enter First Name : ");
             string first_name = Console.ReadLine();
             Console.WriteLine(p.FirstNameVal(first_name));
+            Console.Write("Enter Last Name : ");
+            string last_name = Console.ReadLine();
+            Console.WriteLine(p.LastNameVal(last_name));
+            Console.Write("Enter E-mail : ");
+            string email = Console.ReadLine();
+            Console.WriteLine(p.EmailValidation(email));
+            Console.Write("Enter Mobile_NO : ");
+            string mobile = Console.ReadLine();
+            Console.WriteLine(p.MobileValidation(mobile));
+            Console.Write("Enter Password : ");
+            string password = Console.ReadLine();
+            Console.WriteLine(p.PasswordRule(password));
         }
         public bool FirstNameVal(string fname)
         {
             Regex rg = new Regex(REGEX_PATTERN);
             return rg.IsMatch(fname); ;
+        }
+        public bool LastNameVal(string last_name)
+        {
+            Regex rg = new Regex(REGEX_PATTERN);
+            bool validate = rg.IsMatch(last_name);
+            return validate;
+        }
+        public bool EmailValidation(string email)
+        {
+            string EMAIL_PATTERN = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*(\\@)[a-zA-Z0-9]+(\\.)[a-z]{2,3}[.(a-z)]*$";
+            Regex rg = new Regex(EMAIL_PATTERN);
+            bool validate = rg.IsMatch(email);
+            return validate;
+        }
+        public bool MobileValidation(string mobile)
+        {
+            string MobilePattern = "^(91){1}[ ]+[0-9]{10}$";
+            Regex rg = new Regex(MobilePattern);
+            bool validate = rg.IsMatch(mobile);
+            return validate;
+        }
+        public bool PasswordRule(string password)
+        {
+            string PasswordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*.-])[a-zA-Z0-9].{8,}$";
+            Regex rg = new Regex(PasswordPattern);
+            bool validate = rg.IsMatch(password);
+            return validate;
         }
     }
 }
