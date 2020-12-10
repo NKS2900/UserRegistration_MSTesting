5d8c117e54a08ca1b4decd3d62dd9afe69f5b196
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-10 08:54:03 +0530

    [NIJAM] Add:UserRegistration Testing Using MSTest.

---
+++ UserRegTesting/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistration;
+namespace UserRegTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly Program program;
+        public UnitTest1()
+        {
+            program = new Program();
+        }
+    }
+}
---
+++ UserRegistration/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace UserRegistration
+{
+    public class Program
+    {
+        public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        public static Program p = new Program();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome_To_UserRegistration_RegeEx");
+        }
+    }
+}
