f959bfa429cd4cd2fe2f2d82e58b255a361007dd
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-12-13 23:25:08 +0530

    [NIJAM] Add: Test to Validate The User Entrys

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,61 +1,28 @@
-﻿using System;
+using System;
 using System.Text.RegularExpressions;
 namespace UserRegistration
 {
     public class Program
     {
-        public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}$";
-        public static Program p = new Program();
+        public static RegExValidation regex = new RegExValidation();
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome_To_UserRegistration_RegeEx");
             Console.Write("Enter First Name : ");
             string first_name = Console.ReadLine();
-            Console.WriteLine(p.FirstNameVal(first_name));
+            Console.WriteLine(regex.FirstNameVal(first_name));
             Console.Write("Enter Last Name : ");
             string last_name = Console.ReadLine();
-            Console.WriteLine(p.LastNameVal(last_name));
+            Console.WriteLine(regex.LastNameVal(last_name));
             Console.Write("Enter E-mail : ");
             string email = Console.ReadLine();
-            Console.WriteLine(p.EmailValidation(email));
+            Console.WriteLine(regex.EmailValidation(email));
             Console.Write("Enter Mobile_NO : ");
             string mobile = Console.ReadLine();
-            Console.WriteLine(p.MobileValidation(mobile));
+            Console.WriteLine(regex.MobileValidation(mobile));
             Console.Write("Enter Password : ");
             string password = Console.ReadLine();
-            Console.WriteLine(p.PasswordRule(password));
-        }
-        public bool FirstNameVal(string fname)
-        {
-            Regex rg = new Regex(REGEX_PATTERN);
-            return rg.IsMatch(fname); ;
-        }
-        public bool LastNameVal(string last_name)
-        {
-            Regex rg = new Regex(REGEX_PATTERN);
-            bool validate = rg.IsMatch(last_name);
-            return validate;
-        }
-        public bool EmailValidation(string email)
-        {
-            string EMAIL_PATTERN = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*(\\@)[a-zA-Z0-9]+(\\.)[a-z]{2,3}[.(a-z)]*$";
-            Regex rg = new Regex(EMAIL_PATTERN);
-            bool validate = rg.IsMatch(email);
-            return validate;
-        }
-        public bool MobileValidation(string mobile)
-        {
-            string MobilePattern = "^(91){1}[ ]+[0-9]{10}$";
-            Regex rg = new Regex(MobilePattern);
-            bool validate = rg.IsMatch(mobile);
-            return validate;
-        }
-        public bool PasswordRule(string password)
-        {
-            string PasswordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*.-])[a-zA-Z0-9].{8,}$";
-            Regex rg = new Regex(PasswordPattern);
-            bool validate = rg.IsMatch(password);
-            return validate;
-        }
+            Console.WriteLine(regex.PasswordRule(password));
+        }   
     }
 }
